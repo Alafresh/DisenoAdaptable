adff8f5a3da6fdbccb47d07fcb90f8596ad5b036
juan_ <alafresh16@gmail.com>
2025-05-05 22:14:19 -0500

    add dependencias

---
+++ ReactFactoring/Refactoring/src/App.jsx
@@ -0,0 +1,12 @@
+import Intro from './components/Intro';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Intro />
+    </>
+  );
+}
+
+export default App;
---
+++ ReactFactoring/Refactoring/src/components/Intro.jsx
@@ -0,0 +1,18 @@
+const Intro = () => {
+  return (
+    <div className='hero bg-base-200 min-h-screen'>
+      <div className='hero-content text-center'>
+        <div className='max-w-md'>
+          <h1 className='text-5xl font-bold'>Portafolio</h1>
+          <p className='py-6'>
+            Proyecto de portafolio centrado en el desarrollo de software y
+            experiencias
+          </p>
+          <button className='btn btn-primary'>Entrar</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
