c2faf953ccab07c2a8b9f24dffd1cd4df4111508
juan_ <alafresh16@gmail.com>
2025-05-06 09:45:23 -0500

    finish about me section

--- ReactFactoring/Refactoring/src/components/SobreMi.jsx
+++ ReactFactoring/Refactoring/src/components/SobreMi.jsx
@@ -1,10 +1,33 @@
+import Avatar from '/images/juanito.jpg';
+import Conejos from '/images/conejoides.jpg';
+
 const SobreMi = () => {
   return (
-    <div className='avatar'>
-      <div className='w-46 rounded-full m-10'>
-        <img src='https://img.daisyui.com/images/profile/demo/yellingcat@192.webp' />
+    <section id='about' className='bg-blue-200'>
+      <div className='hero '>
+        <div className='hero-content flex-col lg:flex-row'>
+          <div className='avatar'>
+            <div className='w-46 rounded-full m-10'>
+              <img src={Avatar} />
+            </div>
+          </div>
+          <div>
+            <h1 className='text-5xl font-bold mx-10'>Juan Guillermo Caicedo</h1>
+          </div>
+        </div>
       </div>
-    </div>
+      <div className='hero'>
+        <div className='hero-content flex-col lg:flex-row'>
+          <p className='py-6 text-xl mx-6.5'>
+            Ingeniero en diseño de entretenimiento digital, programador y
+            diseñador de experiencias de usuario y videojuegos. Experiencia en
+            los lenguajes C#, python y Javascript. Y experiencia en los motores
+            graficos de Unity y Unreal.
+          </p>
+          <img src={Conejos} className='max-w-sm rounded-lg shadow-2xl' />
+        </div>
+      </div>
+    </section>
   );
 };
 
