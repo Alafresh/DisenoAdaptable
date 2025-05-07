a6ced67357d589a2f022e562b9ef8e6cfcc84ffa
juan_ <alafresh16@gmail.com>
2025-05-07 00:24:41 -0500

    update

--- ReactFactoring/Refactoring/src/pages/ProjectOne.jsx
+++ ReactFactoring/Refactoring/src/pages/ProjectOne.jsx
@@ -1,9 +1,10 @@
+import NavBarTwo from '../components/NavBarTwo';
+
 const ProjectOne = () => {
   return (
-    <div>
-      <h1>Project One</h1>
-      <p>This is the first project.</p>
-    </div>
+    <>
+      <NavBarTwo />
+    </>
   );
 };
 
