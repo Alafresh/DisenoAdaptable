193799bc425e9c64e3c339bc598e3c2fe78c510c
juan_ <alafresh16@gmail.com>
2025-05-28 08:55:55 -0500

    update animation

--- 3DPortafolio/src/components/GlowCard.jsx
+++ 3DPortafolio/src/components/GlowCard.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
-const GlowCard = ({ card, children }) => {
+const GlowCard = ({ card, children, index }) => {
+  const cardRefs = useRef([]);
+  const handleMouseMove = (index) => (e) => {
+    const card = cardRefs.current[index];
+    if (!card) return;
+
+    // Get the mouse position relative to the card
+    const rect = card.getBoundingClientRect();
+    const mouseX = e.clientX - rect.left - rect.width / 2;
+    const mouseY = e.clientY - rect.top - rect.height / 2;
+
+    // Calculate the rotation angles based on mouse position
+    let angle = Math.atan2(mouseY, mouseX) * (180 / Math.PI);
+    angle = (angle + 360) % 360; // Normalize angle to [0, 360)
+    card.style.setProperty('--start', angle + 60);
+  };
   return (
-    <div className='card card-border timeline-card rounded-xl p-10'>
+    <div
+      ref={(el) => (cardRefs.current[index] = el)}
+      onMouseMove={handleMouseMove(index)}
+      className='card card-border timeline-card rounded-xl p-10'
+    >
       <div className='glow' />
       <div className='flex items-center gap-1 mb-5'>
         {Array.from({ length: 5 }, (_, i) => (
@@ -10,8 +29,9 @@ const GlowCard = ({ card, children }) => {
         ))}
       </div>
       <div className='mb-5'>
-        <p>{card.review}</p>
+        <p className='text-white-50 text-lg'>{card.review}</p>
       </div>
+      {children}
     </div>
   );
 };
