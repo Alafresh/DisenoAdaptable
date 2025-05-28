f3a9dcca00ab1a0507907bae8c5dd261b958a230
juan_ <alafresh16@gmail.com>
2025-05-28 07:58:18 -0500

    add experiencesection

--- 3DPortafolio/src/App.jsx
+++ 3DPortafolio/src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar';
 import FeatureCards from './sections/FeatureCards';
 import Hero from './sections/Hero';
 import ShowCaseSection from './sections/ShowCaseSection';
+import ExperienceSection from './sections/ExperienceSection';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <ShowCaseSection />
       <LogoSection />
       <FeatureCards />
+      <ExperienceSection />
     </>
   );
 };
